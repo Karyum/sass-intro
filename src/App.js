f2122b1c4a2565b2af22dbe0ee907f56e57d8757
Mario <mario.saliba98@gmail.com>
2019-12-08 13:57:18 +0200

    facn7's sass walkthrough project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="title">Hello Everyone</h1>
+        <p className="description">This is a massive description</p>
+      </div>
+      <div className="secondary-container">
+        <h3 className="some-text">Another Random Container</h3>
+      </div>
+    </>
   );
 }
 
